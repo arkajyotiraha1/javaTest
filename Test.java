adcde92093f95d5c6f57019bb5626edb6f55cc2e
Arkajyoti Raha <arkajyotiraha@gmail.com>
2026-01-29 11:07:07 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,42 +1,13 @@
 public class Test {
 
     // Addition
-    static void add(int a, int b) {
-        int res = a + b;
-        System.out.println("Addition op: " + res);
-    }
-
-    // Subtraction
-    static void sub(int a, int b) {
-        int res = a - b;
-        System.out.println("Subtraction op: " + res);
-    }
-
-    // Multiplication
-    static void mul(int a, int b) {
-        int res = a * b;
-        System.out.println("Multiplication op: " + res);
-    }
-
-    // Division
-    static void div(int a, int b) {
-        if (b != 0) {
-            double res = (double) a / b;
-            System.out.println("Division op: " + res);
-        } else {
-            System.out.println("Error: Division by zero is not allowed.");
-        }
-    }
-
-    // Main method
-    public static void main(String[] args) {
-        int x = 2;
-        int y = 2;
-
-        add(x, y);
-        sub(x, y);
-        mul(x, y);
-        div(x, y);
+    public static void main(String[] args){
+        int a = 10;
+            if(a%2==0){
+                System.out.println("Even");
+            }
+            else
+                System.out.println("odd");
     }
 }
 
