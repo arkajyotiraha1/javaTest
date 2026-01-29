459f10a4ee08d1f079ba76cdb5842966597dbe78
Arkajyoti Raha <arkajyotiraha@gmail.com>
2026-01-29 10:59:32 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,26 +3,26 @@ public class Test {
     // Addition
     static void add(int a, int b) {
         int res = a + b;
-        System.out.println("Addition result: " + res);
+        System.out.println("Addition op: " + res);
     }
 
     // Subtraction
     static void sub(int a, int b) {
         int res = a - b;
-        System.out.println("Subtraction result: " + res);
+        System.out.println("Subtraction op: " + res);
     }
 
     // Multiplication
     static void mul(int a, int b) {
         int res = a * b;
-        System.out.println("Multiplication result: " + res);
+        System.out.println("Multiplication op: " + res);
     }
 
     // Division
     static void div(int a, int b) {
         if (b != 0) {
             double res = (double) a / b;
-            System.out.println("Division result: " + res);
+            System.out.println("Division op: " + res);
         } else {
             System.out.println("Error: Division by zero is not allowed.");
         }
